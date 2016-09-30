2ca4c2bed14b2056bc4c9095eb3d47b225993c1c
Reinier <reinier.guerra@salesforce.com>
2016-09-29 22:57:07 -0700

    Adding Spring Framework into the Mix

---
+++ src/main/java/org/polyfill/HelloWorldController.java
@@ -0,0 +1,21 @@
+package org.polyfill;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by reinier.guerra on 9/29/16.
+ */
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String printHello() {
+        return "Hello World";
+    }
+
+}
