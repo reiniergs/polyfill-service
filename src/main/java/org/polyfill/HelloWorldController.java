4386dc8c494cb003708f3fee1d3ee7926249f628
scottmo <scottscmo@gmail.com>
2016-10-12 10:22:07 -0700

    user agent util lib initial implementation

--- src/main/java/org/polyfill/HelloWorldController.java
+++ src/main/java/org/polyfill/HelloWorldController.java
@@ -1,6 +1,10 @@
 package org.polyfill;
 
+import org.polyfill.utils.UserAgent;
+import org.polyfill.utils.UserAgentImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,4 +22,13 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    @RequestMapping(value = "/user-agent-test", method = RequestMethod.GET)
+    @ResponseBody
+    public String userAgentTest(
+            @RequestHeader("User-Agent") String uaString, Model map) {
+        UserAgent ua = new UserAgentImpl(uaString);
+        map.addAttribute("user agent", ua.toString());
+
+        return map.toString();
+    }
 }
