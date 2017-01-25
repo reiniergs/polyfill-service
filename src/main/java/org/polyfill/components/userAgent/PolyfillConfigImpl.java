1e6954ecfcee40fd57f011672868e2eb6a4f5eea
Reinier Guerra <reiniergs@gmail.com>
2017-01-24 17:56:56 -0800

    adding spring config logic (#27)

---
+++ src/main/java/org/polyfill/components/userAgent/PolyfillConfigImpl.java
@@ -0,0 +1,11 @@
+package org.polyfill.components.userAgent;
+
+import org.polyfill.interfaces.PolyfillConfig;
+
+/**
+ * Created by reinier.guerra on 1/24/17.
+ */
+public class PolyfillConfigImpl implements PolyfillConfig {
+    public PolyfillConfigImpl(String path) {
+    }
+}
---
+++ src/main/java/org/polyfill/interfaces/PolyfillConfig.java
@@ -0,0 +1,8 @@
+package org.polyfill.interfaces;
+
+/**
+ * Created by reinier.guerra on 1/24/17.
+ */
+public interface PolyfillConfig {
+
+}
