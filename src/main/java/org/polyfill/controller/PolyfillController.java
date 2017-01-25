12f38d4015a606081c388822cf1ed65ab9ce2d34
Reinier Guerra <reiniergs@gmail.com>
2017-01-24 17:28:56 -0800

    add tsort and routing (#26)

--- src/main/java/org/polyfill/controller/PolyfillController.java
+++ src/main/java/org/polyfill/controller/PolyfillController.java
@@ -1,11 +1,14 @@
-package org.polyfill.controllers;
+package org.polyfill.controller;
 
 import org.polyfill.interfaces.UserAgent;
 import org.polyfill.services.UserAgentParserService;
+import org.polyfill.views.BadRequestView;
 import org.polyfill.views.HandlebarView;
+import org.polyfill.views.PolyfillsView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +18,28 @@ import org.springframework.web.servlet.View;
  * Created by reinier.guerra on 10/12/16.
  */
 @Controller
-public class polyfillController {
+public class PolyfillController {
 
     @Autowired
     UserAgentParserService userAgentParserService;
+
+    @RequestMapping(value = "/polyfill.{type}", method = RequestMethod.GET)
+    public View polyfillApi(@RequestHeader("User-Agent") String uaString,
+                            @PathVariable String type) {
+        if (type.equals("js")) {
+            return new PolyfillsView("Here goes the polyfill implementation.");
+        } else
+            return new BadRequestView("Sorry we just support javascript polyfills.");
+    }
+
+    @RequestMapping(value = "/polyfill.min.{type}", method = RequestMethod.GET)
+    public View polyfillMinApi(@RequestHeader("User-Agent") String uaString,
+                               @PathVariable String type) {
+        if (type.equals("js")) {
+            return new PolyfillsView("Here goes the polyfill minify implementation");
+        } else
+            return new BadRequestView("Sorry we just support javascript polyfills.");
+    }
 
     @RequestMapping(value = "/user-agent", method = RequestMethod.GET)
     public View userAgentTest(@RequestHeader("User-Agent") String uaString, Model model) {
---
+++ src/main/java/org/polyfill/views/PolyfillsView.java
@@ -0,0 +1,31 @@
+package org.polyfill.views;
+
+import org.springframework.web.servlet.View;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by reinier.guerra on 10/12/16.
+ */
+
+public class PolyfillsView implements View {
+    private String polyfills;
+
+    public PolyfillsView(String polyfills) {
+        this.polyfills = polyfills;
+    }
+
+    public String getContentType() {
+        return "text/javascript;charset=UTF-8";
+    }
+
+    public void render(Map<String, ?> map, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ServletOutputStream stream = httpServletResponse.getOutputStream();
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        stream.print(polyfills);
+    }
+}
