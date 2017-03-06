e1c3b8783c2cdd6c58184e6fecb327b658c0c42c
scottmo <scottscmo@gmail.com>
2017-03-06 00:04:50 -0800

    Make ua parser makes use of AppConfig

--- src/test/java/org/polyfill/services/UADetectorBasedUserAgentParserServiceTest.java
+++ src/test/java/org/polyfill/services/UADetectorBasedUserAgentParserServiceTest.java
@@ -1,53 +1,39 @@
 package org.polyfill.services;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.polyfill.interfaces.ConfigLoaderService;
+import org.junit.runner.RunWith;
+import org.polyfill.configurations.MockBrowserAliasesConfig;
 import org.polyfill.interfaces.UserAgent;
+import org.polyfill.interfaces.UserAgentParserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        loader=AnnotationConfigContextLoader.class,
+        classes={MockBrowserAliasesConfig.class,
+                UADetectorBasedUserAgentParserService.class
+        })
 public class UADetectorBasedUserAgentParserServiceTest {
 
-    private Map<String, String> UAStrings;
+    private Map<String, String> UAStrings = new HashMap<String, String>(){{
+        put("chrome", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36");
+        put("firefox_namoroka", "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2a2pre) Gecko/20090908 Ubuntu/9.04 (jaunty) Namoroka/3.6a2pre GTB5 (.NET CLR 3.5.30729)");
+        put("chrome_ios", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/53.0.2785.109 Mobile/14A456 Safari/601.1.46");
+        put("firefox_ios", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) FxiOS/5.3 Mobile/14B100 Safari/602.2.14");
+        put("opera_ios", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) OPiOS/14.0.0.104835 Mobile/14B100 Safari/9537.53");
+        put("aol", "Mozilla/5.0 (compatible; MSIE 9.0; AOL 9.7; AOLBuild 4343.19; Windows NT 6.1; WOW64; Trident/5.0; FunWebProducts)");
+    }};
 
-    @InjectMocks
-    private UADetectorBasedUserAgentParserService uaUtilService;
-
-    @Mock
-    private ConfigLoaderService configLoaderService;
-
-    @Before
-    public void setup() throws IOException {
-        // init all sample UA strings
-        UAStrings = new HashMap<String, String>();
-        UAStrings.put("chrome", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36");
-        UAStrings.put("firefox_namoroka", "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2a2pre) Gecko/20090908 Ubuntu/9.04 (jaunty) Namoroka/3.6a2pre GTB5 (.NET CLR 3.5.30729)");
-        UAStrings.put("chrome_ios", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/53.0.2785.109 Mobile/14A456 Safari/601.1.46");
-        UAStrings.put("firefox_ios", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) FxiOS/5.3 Mobile/14B100 Safari/602.2.14");
-        UAStrings.put("opera_ios", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) OPiOS/14.0.0.104835 Mobile/14B100 Safari/9537.53");
-        UAStrings.put("aol", "Mozilla/5.0 (compatible; MSIE 9.0; AOL 9.7; AOLBuild 4343.19; Windows NT 6.1; WOW64; Trident/5.0; FunWebProducts)");
-
-        // setup mock relationship based on the above annotations
-        MockitoAnnotations.initMocks(this);
-
-        // init test browser family alias map for ua parser
-        Map<String, Object> userAgentAliases = new HashMap<>();
-        userAgentAliases.put("mobile safari", "ios_saf");
-        userAgentAliases.put("firefox (namoroka)", "firefox");
-        Mockito.when(configLoaderService.getConfig(Mockito.anyString())).thenReturn(userAgentAliases);
-
-        // load the mock into ua service
-        uaUtilService.loadUserAgentAliases();
-    }
+    @Autowired
+    private UserAgentParserService uaUtilService;
 
     @Test
     public void testGeneralUAInfo() {
