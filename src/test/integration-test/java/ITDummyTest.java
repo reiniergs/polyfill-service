09be16dbfd67c2ede4c3fc5521f18775d2c14450
Bhargav Venkataraman <bvenkataraman@salesforce.com>
2017-03-28 15:20:29 -0700

    Creating different profiles for running integration tests

---
+++ src/test/integration-test/java/ITDummyTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Bhargav Venkataraman
+ */
+public class ITDummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
