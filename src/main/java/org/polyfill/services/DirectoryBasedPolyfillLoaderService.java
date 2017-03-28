7a87b225b57db6f0530e20c0971ad7e3857eedde
scottmo <scottscmo@gmail.com>
2017-03-28 00:01:53 -0700

    Add necessary import for PolyillLoader

--- src/main/java/org/polyfill/services/DirectoryBasedPolyfillLoaderService.java
+++ src/main/java/org/polyfill/services/DirectoryBasedPolyfillLoaderService.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
