0589515f7a09f7d0f6484ab4bf5dc444fd69d601
Shaocong(Scott) Mo <scottmo@users.noreply.github.com>
2017-05-16 13:49:23 -0700

    Scottmo/modules test (#122)
    * Separate api and web packages
    User can do @Import(ServiceConfig.class) in their configuration class to load api classes
    Make service implementations private to package
    Make use of Query object in PolyfillViews
    
    * uppercase README.md
    
    * Move polyfills to api
    
    * api module
    change loading mechanisms to use resources
    
    * web app module setup
    
    * modules update
    
    * Move shared plugins to parent pom
    
    * Fix loading polyfills from jar
    
    * api module cleanup
    
    * Fix docs
    
    * - Use 404.html instead of handlebars
    - Use handblebar-springmvc to load templates
      - removed unnecessary templates and custom views
    - Fix most view issues
    
    * Fix integration tests
    
    * Remove unnecessary config file. Test can load dispatcher xml to load real context.
    
    * Fix handlebars template not recompiled after the first load.
    
    * React app has its own package.json.
    
    * Add tasks folder
    
    * Set packaging and correct path
    
    * Fix profiles.
    
    * Update polyfills
    
    * Clean up react app's package.json
    Fix webpack.config.js

---
+++ api/src/main/java/org/polyfill/components/Query.java
@@ -0,0 +1,84 @@
+package org.polyfill.components;
+
+import org.polyfill.interfaces.UserAgent;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by smo on 3/28/17.
+ */
+public class Query {
+    // features to request
+    private List<Feature> features;
+
+    // filters
+    private UserAgent userAgent = null;
+    private Set<String> excludes = new HashSet<>();
+    private boolean doLoadOnUnknownUA = false;
+    private boolean doMinify = false;
+
+    // extra options
+    private boolean doIncludeDependencies = true;
+
+    public Query(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<Feature> getFeatures() {
+        return this.features;
+    }
+
+    public UserAgent getUserAgent() {
+        return this.userAgent;
+    }
+
+    public Query setUserAgent(UserAgent userAgent) {
+        this.userAgent = userAgent;
+        return this;
+    }
+
+    public Set<String> getExcludes() {
+        return this.excludes;
+    }
+
+    public Query excludeFeatures(String ... features) {
+        for (String feature : features) {
+            this.excludes.add(feature);
+        }
+        return this;
+    }
+
+    public Query excludeFeatures(List<String> features) {
+        this.excludes.addAll(features);
+        return this;
+    }
+
+    public boolean doLoadOnUnknownUA() {
+        return this.doLoadOnUnknownUA;
+    }
+
+    public Query setLoadOnUnknownUA(boolean doLoadOnUnknownUA) {
+        this.doLoadOnUnknownUA = doLoadOnUnknownUA;
+        return this;
+    }
+
+    public boolean doIncludeDependencies() {
+        return this.doIncludeDependencies;
+    }
+
+    public Query setIncludeDependencies(boolean doIncludeDependencies) {
+        this.doIncludeDependencies = doIncludeDependencies;
+        return this;
+    }
+
+    public boolean doMinify() {
+        return doMinify;
+    }
+
+    public Query setMinify(boolean doMinify) {
+        this.doMinify = doMinify;
+        return this;
+    }
+}
---
+++ api/src/test/java/org/polyfill/configurations/MockPolyfillsConfig.java
@@ -0,0 +1,104 @@
+package org.polyfill.configurations;
+
+import org.polyfill.components.Polyfill;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by smo on 3/4/17.
+ */
+@Configuration
+public class MockPolyfillsConfig {
+
+    @Bean
+    public Map<String, Object> aliases() {
+        return new HashMap<String, Object>(){{
+            put("default", Arrays.asList("a", "b", "c"));
+            put("es6", Arrays.asList("b", "c", "d"));
+            put("foo", Arrays.asList("c", "e"));
+        }};
+    }
+
+    @Bean
+    public Map<String, Object> browserBaselines() {
+        return new HashMap<String, Object>(){{
+            put("chrome", "*");
+            put("firefox", ">=3.6");
+        }};
+    }
+
+    @Bean
+    public Map<String, Polyfill> polyfills() {
+        Polyfill polyfillA = new Polyfill.Builder("a")
+            .aliases(Arrays.asList("default", "foo"))
+            .browserRequirements(new HashMap<String, String>(){{
+                put("chrome", "*");
+                put("firefox", "4 - 31");
+            }})
+            .dependencies(Arrays.asList("b", "d"))
+            .detectSource("a.detectSource")
+            .minSource("a.min")
+            .rawSource("a.raw")
+            .build();
+
+        Polyfill polyfillB = new Polyfill.Builder("b")
+            .aliases(Arrays.asList("default", "es6"))
+            .browserRequirements(new HashMap<String, String>(){{
+                put("chrome", "*");
+                put("firefox", "31");
+            }})
+            .dependencies(Arrays.asList("c"))
+            .detectSource("b.detectSource")
+            .minSource("b.min")
+            .rawSource("b.raw")
+            .build();
+
+        Polyfill polyfillC = new Polyfill.Builder("c")
+            .aliases(Arrays.asList("default", "es6", "foo"))
+            .browserRequirements(new HashMap<String, String>(){{
+                put("chrome", "*");
+                put("firefox", "4 - 31");
+            }})
+            .dependencies(new ArrayList<String>())
+            .detectSource("c.detectSource")
+            .minSource("c.min")
+            .rawSource("c.raw")
+            .build();
+
+        Polyfill polyfillD = new Polyfill.Builder("d")
+            .aliases(Arrays.asList("es6"))
+            .browserRequirements(new HashMap<String, String>(){{
+                put("chrome", "<50");
+                put("firefox", "31");
+            }})
+            .dependencies(Arrays.asList("b", "c"))
+            .detectSource("d.detectSource")
+            .minSource("d.min")
+            .rawSource("d.raw")
+            .build();
+
+        Polyfill polyfillE = new Polyfill.Builder("e")
+            .aliases(Arrays.asList("foo"))
+            .browserRequirements(new HashMap<String, String>(){{
+                put("chrome", "*");
+            }})
+            .dependencies(new ArrayList<>())
+            .detectSource("e.detectSource")
+            .minSource("e.min")
+            .rawSource("e.raw")
+            .build();
+
+        return new HashMap<String, Polyfill>(){{
+            put(polyfillA.getName(), polyfillA);
+            put(polyfillB.getName(), polyfillB);
+            put(polyfillC.getName(), polyfillC);
+            put(polyfillD.getName(), polyfillD);
+            put(polyfillE.getName(), polyfillE);
+        }};
+    }
+}
