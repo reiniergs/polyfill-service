a82e392b2bc455f45f30108ff714c0fc363353fe
scottmo <scottscmo@gmail.com>
2017-05-19 14:22:06 -0700

    Add inputStream support for query loader. User can now use class resource and other inputStream to load query configuration as well as file path.

--- api/src/main/java/org/polyfill/api/interfaces/QueryLoaderService.java
+++ api/src/main/java/org/polyfill/api/interfaces/QueryLoaderService.java
@@ -1,6 +1,10 @@
 package org.polyfill.api.interfaces;
 
 import org.polyfill.api.components.Query;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by smo on 4/21/17.
@@ -11,6 +15,16 @@ public interface QueryLoaderService {
      * The query object will be used for getting polyfills
      * @param filePath file path to the configuration file
      * @return query object
+     * @throws IOException if resource doesn't exist, is invalid, or cannot be parsed
      */
-    Query loadQuery(String filePath);
+    Query loadQuery(String filePath) throws IOException;
+
+    /**
+     * Load query configuration from an input stream
+     * The query object will be used for getting polyfills
+     * @param inputStream input stream of query configuration
+     * @return query object
+     * @throws IOException if inputStream is invalid, or cannot be parsed
+     */
+    Query loadQuery(InputStream inputStream) throws IOException;
 }
