5eae9990c9f9dea7ab657f3c29bdb8c3c75f1f06
scottmo <scottscmo@gmail.com>
2017-08-06 11:28:49 -0700

    Use == null checks in getPrototypeOf

--- polyfill-service-api/polyfills/Object/getPrototypeOf/polyfill.js
+++ polyfill-service-api/polyfills/Object/getPrototypeOf/polyfill.js
@@ -1,9 +1,9 @@
 (function(_getPrototypeOf, Object) {
 	Object.getPrototypeOf = function getPrototypeOf(o) {
-		if (o === null || o === undefined) {
+		if (o == null) {
 			throw new TypeError("Cannot convert undefined or null to object");
 		}
-		if (_getPrototypeOf === undefined) {
+		if (_getPrototypeOf == null) {
 			return object.constructor ? object.constructor.prototype : null;
 		}
 		return _getPrototypeOf(Object(o));
