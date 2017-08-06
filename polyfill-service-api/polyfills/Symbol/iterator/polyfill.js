fc2f46e8c23736b150ccd0a3f0e7d0970ac4a9bb
scottmo <scottscmo@gmail.com>
2017-08-05 19:04:08 -0700

    Update Symbol polyfills

--- polyfill-service-api/polyfills/Symbol/iterator/polyfill.js
+++ polyfill-service-api/polyfills/Symbol/iterator/polyfill.js
@@ -1,1 +1,51 @@
 Object.defineProperty(Symbol, 'iterator', {value: Symbol('iterator')});
+
+(function (Si, AP, SP) {
+
+	function returnThis() { return this; }
+
+	// make Arrays usable as iterators
+	// so that other iterables can copy same logic
+	if (!AP[Si]) AP[Si] = function () {
+		var
+			i = 0,
+			self = this,
+			iterator = {
+				next: function next() {
+					var done = self.length <= i;
+					return done ?
+						{done: done} :
+						{done: done, value: self[i++]};
+				}
+			}
+		;
+		iterator[Si] = returnThis;
+		return iterator;
+	};
+
+	// make Strings usable as iterators
+	// to simplify Array.from and for/of like loops
+	if (!SP[Si]) SP[Si] = function () {
+		var
+			fromCodePoint = String.fromCodePoint,
+			self = this,
+			i = 0,
+			length = self.length,
+			iterator = {
+				next: function next() {
+					var
+						done = length <= i,
+						c = done ? '' : fromCodePoint(self.codePointAt(i))
+					;
+					i += c.length;
+					return done ?
+						{done: done} :
+						{done: done, value: c};
+				}
+			}
+		;
+		iterator[Si] = returnThis;
+		return iterator;
+	};
+
+}(Symbol.iterator, Array.prototype, String.prototype));
