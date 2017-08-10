7fbb5cf9041278c26039e73bf753c82bdba63734
Diego Ferreiro Val <elfogris@gmail.com>
2017-08-09 18:04:46 -0700

    Fixing IE11 Symbol polyfill issue
    Function `toString` was expected to be declared within the same scope, but because the polyfill service creates its own closure, its getting the wrong scoped version.

--- polyfill-service-api/polyfills/Symbol/toStringTag/polyfill.js
+++ polyfill-service-api/polyfills/Symbol/toStringTag/polyfill.js
@@ -1,11 +1,12 @@
 (function() {
+	var objToString = Object.prototype.toString;
 	Object.defineProperty(Symbol, 'toStringTag', {
 		value: Symbol('toStringTag')
 	});
 
 	var descriptor = Object.getOwnPropertyDescriptor(Object.prototype, 'toString');
 	descriptor.value = function () {
-		var str = toString.call(this),
+		var str = objToString.call(this),
 			tst = this[Symbol.toStringTag]
 		;
 		return typeof tst === 'undefined' ? str : ('[object ' + tst + ']');
